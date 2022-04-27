bc4b932894641419bbc11f47fea89d3668a95446
Reinaldo Solano <reinaldo.solano@antikytera.com>
2022-04-27 07:32:11 -0300

    alterado cli

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,13 @@
-from .config import settings
+import typer
 
+main = typer.Typer(help="Beer Management Application")
 
-def main():
-    print("Hello from", settings.NAME)
+@main.command("add")
+def add(name: str, style: str):
+    """Adds a new beer to database."""
+    print(name, style)
+
+@main.command("list")
+def list_beers(style: str):
+    """Lists beers in database."""
+    print(style)
